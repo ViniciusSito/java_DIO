408cc9e2ddacdda53ea1d8b3863a47edf890dd6c
ViniciusSito <itoviniciusshigueyuki@gmail.com>
2026-08-26 21:41:03 -0300

    aula basica de javascript, iniciando do basico desde conceito ate os operadores

---
+++ Javascript/aula2/script.js
@@ -0,0 +1,82 @@
+/*
+//boolean
+var boolean = false;
+console.log(typeof boolean);
+
+//number
+var number = 1;
+console.log(typeof number);
+
+//string
+var string = "Nome novo";
+console.log(typeof string);
+
+var variavel =  "Vinicius";
+variavel = 10;
+console.log("variavel: " + variavel);
+
+let variavel2 = "nome novo";
+variavel2 = "nome antigo"
+console.log("variavel2: " + variavel2);
+
+const constante = "nome constante";
+constante = "nome constante alterado";
+console.log("constante: " + constante);
+
+var escopoGlobal = "global";
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = "local";
+    console.log(escopoLocalInterno);
+}
+escopoLocal();
+
+
+var atribuicao = "nome";
+
+var comparacao = "10" == 0;
+console.log(comparacao);
+
+var comparacaoIgualdade = "10" === 0;
+console.log(comparacaoIgualdade);
+*/
+
+var adicao = 1 + 1;
+console.log(adicao);
+
+var subtracao = 1 - 1;
+console.log(subtracao);
+
+var multiplicacao = 2 * 2;
+console.log(multiplicacao);
+
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
+
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);
+
+var e = true && false;
+console.log(e);
+
+var ou = 5 == 5 || 4 == 4;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
